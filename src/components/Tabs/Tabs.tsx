dedad2537b8311975376494a046fbc9977fe0157
卓燕珠 <zhuoyanzhu@zhuoyanzhudeMacBook-Pro-2.local>
2020-06-05 11:37:15 +0800

    💄 UI: Tab component

---
+++ src/components/Tabs/Tabs.tsx
@@ -0,0 +1,29 @@
+import React, { ReactNode } from 'react';
+import styles from './Tabs.module.scss';
+import clsx from 'clsx';
+
+interface Tabs {
+    id: string;
+    title: string;
+}
+export interface OwnProps {
+    tabs: Tabs[];
+    onSelect: (id: string) => void;
+    selected: string;
+}
+export default function Tabs(props:OwnProps) {
+    return (
+        <ul className={styles.tabs_container}>
+            {props.tabs.map((tab) => (
+                <li className={clsx(styles.tab, {[styles.selected]: props.selected === tab.id})}
+                    onClick={() => {
+                        console.log('tab.id', tab.id)
+                        props.onSelect(tab.id)
+                    }}
+                    key={tab.id}>
+                        {tab.title}
+                </li>
+            ))} 
+        </ul>
+    )
+}
