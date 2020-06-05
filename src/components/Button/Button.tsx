21520a18f55599ddff0bb2ad01b6c0b040822f5e
卓燕珠 <zhuoyanzhu@zhuoyanzhudeMacBook-Pro-2.local>
2020-06-05 11:36:22 +0800

    💄 UI: Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from 'react';
+import clsx from 'clsx';
+import styles from './Button.module.scss';
+
+interface Props {
+    children: ReactNode;
+    type?: 'primary'|'default';
+    onClick: VoidFunction;
+    className?:string;
+}
+export default function Button({children, type = 'default', onClick, className} : Props) {
+    return (
+        <button className={clsx(styles.button, styles[type], styles.button, className)} onClick={onClick}>
+            {children}
+        </button>
+    )
+}
