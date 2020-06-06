4a76deb960573078fc9cd9ce7f8aeecd8689d8a4
卓燕珠 <zhuoyanzhu@zhuoyanzhudeMacBook-Pro-2.local>
2020-06-06 23:57:05 +0800

    💄 UI: fix styles of Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,37 +1,37 @@
 import React from 'react';
 import style from './Home.module.scss';
-import {BillListItem} from 'store/types/bills';
+import { BillListItem } from 'store/types/bills';
 import Select from 'components/Select';
 import Button from 'components/Button';
 import BillList from './components/BillList';
 import Summary from './components/Summary';
 import useFilterBills from './useFilterBills';
-import {months} from 'utils/data';
+import { months } from 'utils/data';
 
 export interface StateProps {
   bills: BillListItem[];
 }
 
 export interface DispatchProps {
-    gotoAddBill: VoidFunction;
+  gotoAddBill: VoidFunction;
 }
 
-export interface OwnProps {}
+export interface OwnProps { }
 
 type Props = OwnProps & StateProps & DispatchProps;
 
-function Home(props:Props) {
-  const {filterBillList ,filterMonth, handleMonth, billStatistics} = useFilterBills(props.bills);
+function Home(props: Props) {
+  const { filterBillList, filterMonth, handleMonth, billStatistics } = useFilterBills(props.bills);
 
   return (
     <div className={style.page}>
-      <Summary billStatistics={billStatistics}/>
+      <Summary billStatistics={billStatistics} />
       <div className={style.container}>
         <div className={style.header}>
-          <p>账单记录</p>
+          <span>账单记录</span>
           <Select name="month" value={filterMonth} onChange={handleMonth} options={months}></Select>
         </div>
-        <BillList billList={filterBillList}/>
+        <BillList billList={filterBillList} />
       </div>
       <Button onClick={props.gotoAddBill} type="primary" className={style.button}>+</Button>
     </div>
